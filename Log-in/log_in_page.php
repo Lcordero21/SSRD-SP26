888909c5433b5f95d7035d944bd6de639d61562c
Lcordero21 <lcordero@willamette.edu>
2026-03-28 13:08:06 -0700

    test

--- Log-in/log_in_page.php
+++ Log-in/log_in_page.php
@@ -6,6 +6,19 @@
     <title>Log In Page</title>
 </head>
 <body>
-    
+    <form>
+        <div class="mb-3">
+            <label for="InputEmail" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp">
+            <div id="emailHelp" class="form-text">Please Enter Your Willamette University Email!</div>
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Password</label>
+            <input type="password" class="form-control" id="exampleInputPassword1">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>    
+    </form>
 </body>
 </html>
