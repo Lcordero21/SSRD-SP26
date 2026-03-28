a87d752c1eb34b530756cb849d87728f08b3f51f
Lcordero21 <lcordero@willamette.edu>
2026-03-28 13:33:13 -0700

    log in page

--- Log-in/log_in_page.php
+++ Log-in/log_in_page.php
@@ -3,9 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="log_in_page.css">
     <title>Log In Page</title>
 </head>
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#"><img class = "compass_logo" src="../images/compass-4c1.jpg"/></a>
+        </div>
+    </nav>
+
+    <div class="row">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
     <form>
         <div class="mb-3">
             <label for="InputEmail" class="form-label">Email address</label>
@@ -17,8 +30,20 @@
             <label for="exampleInputPassword1" class="form-label">Password</label>
             <input type="password" class="form-control" id="exampleInputPassword1">
         </div>
-        
+
         <button type="submit" class="btn btn-primary">Submit</button>    
     </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
 </body>
 </html>
+
+<php>
+
+
+
+</php>
